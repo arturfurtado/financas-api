3f1a01341a4e4640812223818c5e21188cba5da8
arturfurtado <artururtado@gmail.com>
2025-03-15 16:06:29 -0300

    testing git config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,31 @@
-import 'reflect-metadata';
-import Application from "./app"
+import "reflect-metadata";
+import { AppDataSource } from "./plugins/db";
+import fastify, { FastifyInstance } from "fastify";
+import config from "./config";
+class Application {
+  server: FastifyInstance;
 
-Application.main();
+  constructor() {
+    this.server = fastify();
+  }
+
+  async startHttpServer() {
+    try {
+      const address = await this.server.listen({
+        port: config.port,
+      });
+      console.log(`Servidro rodando na porta: ${address}`);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  }
+
+  async main() {
+    await AppDataSource.initialize();
+    await this.startHttpServer();
+  }
+}
+
+const appInstance = new Application();
+appInstance.main();
---
+++ src/routes/login.ts
@@ -0,0 +1,10 @@
+// import { fastify } from "fastify";
+
+// const app = fastify();
+
+// app.route({
+//     method: 'POST',
+//     url: '/login',
+    
+
+// })
